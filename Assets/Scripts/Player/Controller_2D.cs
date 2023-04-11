6d7c7ccd5e38a4689d27c347cfbca86c728a796f
Max-Emilien Allègre <maxemilien.frg@gmail.com>
2023-04-11 12:03:00 +0200

    animation_et_equilibrage

--- Assets/Scripts/Player/Controller_2D.cs
+++ Assets/Scripts/Player/Controller_2D.cs
@@ -9,6 +9,9 @@ public class Controller_2D : MonoBehaviour
     CapsuleCollider2D cap;
     Animator anim_controller;
     public Player_Health player_health;
+    public Health_Bar health_bar;
+    public Player_Gauge player_gauge;
+    public Gauge_Bar gauge_bar;
 
     [Header("Movement Settings")]
     [SerializeField] float moveSpeed_horizontal;
@@ -17,10 +20,11 @@ public class Controller_2D : MonoBehaviour
     int direction;
     Vector2 ref_velocity = Vector2.zero;
     bool facing_right = true;
-    float jumpForce = 22.5f;
+    float jumpForce = 23.5f;
     bool is_jumping = false;
 
     [Header("Status Settings")]
+    Vector2 respawn_point = new Vector2 (-7, 2.5f);
     [SerializeField] bool grounded;
     [SerializeField] Transform ground_check;
     [SerializeField] LayerMask what_is_ground;
@@ -28,14 +32,12 @@ public class Controller_2D : MonoBehaviour
     [SerializeField] Transform front_check;
     [SerializeField] bool is_touching_up;
     [SerializeField] Transform up_check;
-    [SerializeField] bool is_crouching;
     [SerializeField] bool wall_sliding;
     [SerializeField] bool is_attacking;
     [SerializeField] bool is_guarding;
     [SerializeField] bool is_dashing;
     [SerializeField] bool is_waiting;
     float check_radius = 0.1f;
-    public Player_Gauge player_gauge;
 
     [Header("Attack Settings")]
     [SerializeField] int damage_point = 15;
@@ -80,17 +82,16 @@ public class Controller_2D : MonoBehaviour
         is_touching_front = Physics2D.OverlapCircle(front_check.position, check_radius, what_is_ground); // IS THE PLAYER TOUCHING A WALL ?
         is_touching_up = Physics2D.OverlapCircle(up_check.position, check_radius, what_is_ground); // IS THE PLAYER TOUCHING A CEILING ?
 
-        if (Input.GetButtonDown("Jump") && grounded && !is_crouching && !is_touching_up)
+        if (player_health.current_health <= 0)
+        {
+            StartCoroutine(PlayerDie());
+        }
+
+        if (Input.GetButtonDown("Jump") && grounded && !is_touching_up)
         {
             is_jumping = true;
             Jump();
         }
-
-        if (Input.GetButton("Crouch") && grounded)
-        {
-            is_crouching = true;
-        }
-        else is_crouching = false;
 
         if (Input.GetButtonDown("Attack") && Time.time >= next_attack_time && !wall_sliding && !is_guarding && !is_dashing && !is_waiting)
         {
@@ -122,28 +123,20 @@ public class Controller_2D : MonoBehaviour
         if (is_touching_front && !grounded && horizontal_value != 0)
         {
             wall_sliding = true;
-            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, - wall_sliding_speed, float.MaxValue));
+            anim_controller.SetBool("Wall_Slide", true);
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wall_sliding_speed, float.MaxValue));
         }
-        else wall_sliding = false;
+        else
+        {
+            wall_sliding = false;
+            anim_controller.SetBool("Wall_Slide", false);
+        }
     }
 
     void FixedUpdate()
     {
         Vector2 target_velocity = new Vector2(horizontal_value * moveSpeed_horizontal * Time.deltaTime, rb.velocity.y);
         rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocity, ref ref_velocity, 0.05f); // BASIC MOVEMENT
-        
-        // CROUCH
-        if (is_crouching && grounded)
-        {
-            moveSpeed_horizontal = 200;
-            cap.direction = CapsuleDirection2D.Horizontal;
-        }
-        else if (!is_touching_up)
-        {
-            is_crouching = false;
-            moveSpeed_horizontal = 450;
-            cap.direction = CapsuleDirection2D.Vertical;
-        }
 
         // GUARD
         if (is_guarding && !wall_sliding)
@@ -161,6 +154,14 @@ public class Controller_2D : MonoBehaviour
         if (collision.gameObject.CompareTag("Enemy") && !is_dashing && player_health.current_health > 0)
         {
             player_health.TakeDamage(20);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Checkpoint") && player_health.current_health > 0)
+        {
+            respawn_point = collision.gameObject.transform.position;
         }
     }
 
@@ -218,5 +219,17 @@ public class Controller_2D : MonoBehaviour
         is_waiting = true; // COOLDOWN
         yield return new WaitForSeconds(1);
         is_waiting = false;
+    }
+    IEnumerator PlayerDie()
+    {
+        anim_controller.SetBool("Death", true);
+        yield return new WaitForSeconds(1.5f);
+        anim_controller.SetBool("Death", false);
+        player_health.current_health = player_health.max_health - 30;
+        health_bar.SetHealth(player_health.current_health);
+        player_gauge.current_gauge = 300;
+        gauge_bar.SetGauge(player_gauge.current_gauge);
+        transform.position = respawn_point;
+        yield return new WaitForSeconds(1);
     }
 }
