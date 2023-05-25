46b43185cfc8996ffffeb3890c7136c8b67b067e
Max-Emilien Allègre <maxemilien.frg@gmail.com>
2023-05-25 19:00:46 +0200

    suite_ia_et_rien

--- Assets/Scripts/Enemies/Knight.cs
+++ Assets/Scripts/Enemies/Knight.cs
@@ -18,6 +18,7 @@ public class Knight : MonoBehaviour
     [SerializeField] LayerMask enemy_layers;
     float attack_range = 1.2f;
     float next_attack_time = 0f;
+    bool cooldown;
 
     // Start is called before the first frame update
     void Start()
@@ -29,9 +30,15 @@ public class Knight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, target.position) < 15)
+
+        if (Vector2.Distance(transform.position, target.position) < 15 && Vector2.Distance(transform.position, target.position) > 2.5f && !cooldown && !is_attacking)
         {
             transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, height), speed * Time.deltaTime); // FOLLOWING THE PLAYER
+        }
+
+        if (Vector2.Distance(transform.position, target.position) <= 2.7f && next_attack_time <= Time.time && !is_attacking && !cooldown)
+        {
+            Attack();
         }
     }
 
@@ -48,6 +55,7 @@ public class Knight : MonoBehaviour
             player.GetComponent<Player_Health>().TakeDamage(damage_point); // TAKES THE TakeDamage(int damage) FUNCTION IN THE ENEMY'S SCRIPT TO GIVE DAMAGE TO THE ENEMY
         }
         next_attack_time = Time.time + 2f; // LIMITS THE NUMBER OF ATTACKS AT 4 PER SECONDS
+        cooldown = true;
         is_attacking = false;
     }
 }
