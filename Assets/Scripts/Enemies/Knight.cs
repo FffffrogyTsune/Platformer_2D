c08c5db3f5aa8d132a12f98ea649dfe20bcb6db6
Max-Emilien Allègre <maxemilien.frg@gmail.com>
2023-05-25 20:08:20 +0200

    suite_ia_et_plateformes

--- Assets/Scripts/Enemies/Knight.cs
+++ Assets/Scripts/Enemies/Knight.cs
@@ -11,14 +11,16 @@ public class Knight : MonoBehaviour
     public float height;
     Transform mob_transform;
     [SerializeField] bool is_attacking;
+    [SerializeField] bool stun = false;
 
     [Header("Attack Settings")]
     [SerializeField] int damage_point;
     [SerializeField] Transform attack_point;
     [SerializeField] LayerMask enemy_layers;
-    float attack_range = 1.2f;
+    public float attack_range;
+    public float cooldown_time;
     float next_attack_time = 0f;
-    bool cooldown;
+    float cooldown = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -30,15 +32,20 @@ public class Knight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Time.time >= cooldown)
+        {
+            stun = false;
+        }
 
-        if (Vector2.Distance(transform.position, target.position) < 15 && Vector2.Distance(transform.position, target.position) > 2.5f && !cooldown && !is_attacking)
+        if (Vector2.Distance(transform.position, target.position) < 15 && Vector2.Distance(transform.position, target.position) > 2.5f && !stun && !is_attacking)
         {
             transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, height), speed * Time.deltaTime); // FOLLOWING THE PLAYER
         }
 
-        if (Vector2.Distance(transform.position, target.position) <= 2.7f && next_attack_time <= Time.time && !is_attacking && !cooldown)
+        if (Vector2.Distance(transform.position, target.position) <= 2.7f && Time.time >= next_attack_time && !is_attacking && !stun)
         {
-            Attack();
+            is_attacking = true;
+            StartCoroutine(Attack());
         }
     }
 
@@ -47,15 +54,17 @@ public class Knight : MonoBehaviour
 
     }
 
-    void Attack()
+    IEnumerator Attack()
     {
-        Collider2D[] hit_player = Physics2D.OverlapCircleAll(attack_point.position, attack_range, enemy_layers); // LIST OF ALL ENEMIES HIT
+        yield return new WaitForSeconds(0.7f);
+        Collider2D[] hit_player = Physics2D.OverlapCircleAll(attack_point.position, attack_range, enemy_layers); // DETECTION OF THE PLAYER
         foreach (Collider2D player in hit_player) // IF PLAYER TOUCHED
         {
-            player.GetComponent<Player_Health>().TakeDamage(damage_point); // TAKES THE TakeDamage(int damage) FUNCTION IN THE ENEMY'S SCRIPT TO GIVE DAMAGE TO THE ENEMY
+            player.GetComponent<Player_Health>().TakeDamage(damage_point); // TAKES THE TakeDamage(int damage) FUNCTION IN THE PLAYER'S SCRIPT TO GIVE DAMAGE TO THE ENEMY
         }
-        next_attack_time = Time.time + 2f; // LIMITS THE NUMBER OF ATTACKS AT 4 PER SECONDS
-        cooldown = true;
+        next_attack_time = Time.time + 3f;
+        stun = true;
+        cooldown = Time.time + cooldown_time;
         is_attacking = false;
     }
 }
