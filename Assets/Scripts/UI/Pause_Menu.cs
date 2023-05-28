0bd5ddfd75d687818f2c61ea4985df95b11a1b4f
Max-Emilien Allègre <maxemilien.frg@gmail.com>
2023-05-28 19:24:32 +0200

    menu_pause_et_reglages

---
+++ Assets/Scripts/UI/Pause_Menu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_Menu : MonoBehaviour
+{
+    public static bool paused = false;
+    public GameObject pause_menu_ui;
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (paused) Resume();
+            else Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pause_menu_ui.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    void Pause()
+    {
+        pause_menu_ui.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+
+    public void Menu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+}
